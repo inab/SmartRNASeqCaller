b76596af84bef397bb94cdc02d62aa505e03439e
mbosio85 <mattia.bosio85@gmail.com>
2018-09-27 17:13:58 +0200

    added error message for generate_intron_slice.py

--- resources/generate_intron_slice.py
+++ resources/generate_intron_slice.py
@@ -1,6 +1,13 @@
 import sys
 offset =4
-with open(sys.argv[1]) as rd :
+if len(sys.argv) !=2 :
+    print 'USAGE:'
+    print 'This files takes one bed file as argument with intronic regions: one per line'
+    print 'This will print a new bed file with the 4bp extremities for each intron line'
+    print 'The resulting BED file should be used with SmartRNASeqCaller as intron bed file'
+    print 'Exiting'
+else:
+  with open(sys.argv[1]) as rd :
     for line in rd:
         ff= line.strip().split('\t')
         chr_ = ff[0]
